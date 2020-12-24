27afb52b48d2d6a2668d716f1d7c6b61c7ee5bf0
Jenny Hu <orcas03030@gmail.com>
2020-12-23 19:11:29 -0800

    testing unary

---
+++ lets_go/greet/generate.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+export GOROOT=/usr/local/go
+export GOPATH=$HOME/go
+export GOBIN=$GOPATH/bin
+export PATH=$PATH:$GOROOT:$GOPATH:$GOBIN
+export GOPATH=$HOME/go
+PATH=$PATH:$GOPATH/bin
+
+echo $HOME
+echo $PATH
+
+protoc greetpb/greet.proto --go_out=plugins=grpc:.
+protoc greetpb/greet.proto --go-grpc_out=.
